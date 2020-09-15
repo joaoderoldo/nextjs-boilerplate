1dfbe9e24983dd10d71db641d702c08ec4428470
Joao Deroldo <joao.skr@hotmail.com>
2020-09-15 00:15:26 -0300

    Implemented Storybooks and configured first storybook components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,22 @@
-function Main() {
+import * as S from './styles'
+
+function Main({
+  title = 'React Avançado',
+  description = 'TypeScript, React, NextJS e Styled Components'
+}) {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Desenvolvedor sentado olhando para o computador"
+      />
+    </S.Wrapper>
   )
 }
 

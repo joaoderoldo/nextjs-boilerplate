b61b0fa73ef1a058e857c6594546c22def5e916a
Joao Deroldo <joao.skr@hotmail.com>
2020-09-15 00:27:33 -0300

    Implemented PWA and changed some project informations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>React avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="Boilerplate to work with TypeScript, React, NextJS And Styled Components"
